5e4aecd58b20543fd27084c8892a996812f42559
Benjamin <305193188@qq.com>
2019-08-08 17:31:59 +0800

    设置注释

--- LeetCodeNoteBook/src/com/Benjamin/leetcode/Test.java
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/Test.java
@@ -1,6 +1,14 @@
 package com.Benjamin.leetcode;
 
-
+/**
+ * ClassName:Test
+ * Package:com.Benjamin.leetcode
+ * <p>
+ * Description:
+ *
+ * @author: Benjamin
+ * @date: 19-8-8 下午5:30
+ */
 public class Test {
     public static void main(String[] args) {
 
