3612500ae6a986961b51cf542080de5f52033b77
Benjamin <305193188@qq.com>
2019-09-17 19:18:43 +0800

    ..

--- LeetCodeNoteBook/src/com/Benjamin/leetcode/Test.java
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/Test.java
@@ -14,7 +14,6 @@ import java.util.Date;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println(new Date().getTime());
         System.out.println("hello world, ha?");
     }
 }
