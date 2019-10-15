0bd9ad40ab2dbb7adbe08410412b59a72d9f7fba
Benjamin <305193188@qq.com>
2019-10-15 15:20:19 +0800

    Lexer

--- LeetCodeNoteBook/src/com/Benjamin/leetcode/Lexer.java
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/Lexer.java
@@ -81,21 +81,20 @@ public class Lexer {
     // 对字符串进行分析
     public static int analysis(String s) {
         int sign = 0;
-        boolean flag = false;//标志变量，用来判断是否为数字
+        boolean flag = false;
         if (isReservedWord(s) >= 0) {//是否为保留字
             sign = isReservedWord(s);
             typenum = 1;
         } else {
             for (int k = 0; k < s.length(); k++) {//是否为数字串
                 if (s.substring(k, k + 1).compareTo("0") >= 0 && s.substring(k, k + 1).compareTo("9") <= 0) {
-                    flag = true;//如果一直是数字，则为真
+                    flag = true;
                 } else {
                     flag = false;
-                    break;//当有一个不位数字，则为假
+                    break;
                 }
             }
-            if (flag) {
-                //数字
+            if (flag) {//数字
                 sign = 29;
                 typenum = 5;
             } else {//标识符
@@ -111,34 +110,32 @@ public class Lexer {
         read(path);//读取文件
         System.out.println("InPut:\n" + buffer + "\n\nOutPut:");
         String Str1 = null;
-        int p = 0, typenum1 = 0;
+        int p = 0;
         int sign = 0, num = 0;
         boolean flag1 = true, flag2 = false;
         do {
-            flag2 = false;//标识关系两个字节的关系运算符
-            Str1 = null;//存储提取的一段字符串
-            num = 0;//存储种别码
+            //标识关系两个字节的关系运算符
+            flag2 = false;
+            //存储提取的一段字符串
+            Str1 = null;
+            //存储种别码
+            num = 0;
             while (!buffer.substring(p, p + 1).equals(" ") && !buffer.substring(p, p + 1).equals("\n")) {
-                //读取buffer中的字符，直到遇到换行符或者空格
                 if (isLimiterWord(buffer.substring(p, p + 1)) > 0) {//遇到分隔符
-                    typenum1 = 4;
                     num = isLimiterWord(buffer.substring(p, p + 1));
                     break;
                 }
                 if (isOperateWord(buffer.substring(p, p + 1)) > 0) {//遇到算术符号
-                    typenum1 = 2;
                     num = isOperateWord(buffer.substring(p, p + 1));
                     break;
                 }
                 if (isRelationWord(buffer.substring(p, p + 1)) > 0) {//遇到关系运算符
                     if (buffer.substring(p, p + 1).equals(":")) {
                         if (buffer.substring(p + 1, p + 2).equals("=")) {//遇到:=分隔符
-                            typenum1 = 4;
                             num = 22;
                             break;
                         }
                     }
-                    typenum1 = 3;
                     if (isRelationWord(buffer.substring(p, p + 2)) > 0) {//遇到两个字节的关系运算符
                         flag2 = true;
                         num = isRelationWord(buffer.substring(p, p + 2));
@@ -148,7 +145,7 @@ public class Lexer {
                     break;
                 }
                 if (buffer.substring(p, p + 1).equals("#")) {//遇到结束符号
-                    flag1 = false;//flag1表示着是否遇到结束符号
+                    flag1 = false;
                     break;
                 }
                 if (Str1 == null) {//这是为了区别当Str1为null时，使用+=会造成字符串中多处一个null
@@ -158,8 +155,6 @@ public class Lexer {
                 }
                 p++;
             }
-
-            //输出
 
             if (Str1 != null) {//当提取的字符串不为空时
                 sign = analysis(Str1);//分析字符串的种别码
@@ -179,22 +174,17 @@ public class Lexer {
                 } else {
                     System.out.println("(" + sign + "," + Str1 + ")");
                 }
-//                System.out.println("("+sign+","+Str1+","+type[typenum]+")");
-//                System.out.println("("+sign+","+Str1+")");
             }
             if (num > 0) {//当为特殊符号时(分隔符，算术符号，关系运算符)
                 if (num == 22) {
-//                    System.out.println("(25,:=,分隔符)");
                     System.out.println("(22,:=)");
                     p = p + 2;
                     continue;
                 }
                 if (flag2) {//两个字节的关系运算符
-//                    System.out.println("("+num+","+buffer.substring(p,p+2)+","+type[typenum1]+")");
                     System.out.println("(" + num + "," + buffer.substring(p, p + 2) + ")");
                     p++;
                 } else {
-//                    System.out.println("("+num+","+buffer.substring(p,p+1)+","+type[typenum1]+")");
                     System.out.println("(" + num + "," + buffer.substring(p, p + 1) + ")");
                 }
             }
