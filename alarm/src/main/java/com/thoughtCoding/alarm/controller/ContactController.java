33f300cd318a601dfc3fa35d5211ac1f23ea7660
Benjamin <305193188@qq.com>
2019-10-17 21:54:15 +0800

    alarm SMS短信

--- alarm/src/main/java/com/thoughtCoding/alarm/controller/ContactController.java
+++ alarm/src/main/java/com/thoughtCoding/alarm/controller/ContactController.java
@@ -1,8 +1,17 @@
 package com.thoughtCoding.alarm.controller;
 
+import com.thoughtCoding.alarm.model.Constant;
+import com.thoughtCoding.alarm.model.User;
 import com.thoughtCoding.alarm.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ClassName:ContactController
@@ -17,4 +26,24 @@ import org.springframework.stereotype.Controller;
 public class ContactController {
     @Autowired
     private ContactService contactService;
+
+    @RequestMapping("/addContact")
+    @ResponseBody
+    public Object addContact(@RequestParam(value = "uid", required = false) Short uid,
+                             @RequestParam(value = "callPhone", required = true) String callPhone,
+                             HttpServletRequest request) {
+        Map<String, String> rtn = new HashMap<>();
+
+        // 如果没有传入uid参数(postman测试),从session中获取uid
+        if (uid == null) {
+            uid = ((User) request.getSession().getAttribute(Constant.USER)).getUserId();
+        }
+        if (callPhone.length() != 11) {
+            rtn.put("code", Constant.ERROR);
+            rtn.put("errorMessage", "手机号格式不正确!");
+            return rtn;
+        }
+        // TODO
+
+    }
 }
---
+++ alarm/src/main/java/com/thoughtCoding/alarm/model/SMSConfig.java
@@ -0,0 +1,33 @@
+package com.thoughtCoding.alarm.model;
+
+/**
+ * ClassName:SMSConfig
+ * Package:com.thoughtCoding.alarm.model
+ * <p>
+ * Description:
+ * 调用短信接口所需要的隐私性参数,存储在config.properties文件中
+ *
+ * @author: Benjamin
+ * @date: 19-10-17 下午9:37
+ */
+public class SMSConfig {
+    private String secretId;
+
+    private String secretKey;
+
+    public String getSecretId() {
+        return secretId;
+    }
+
+    public void setSecretId(String secretId) {
+        this.secretId = secretId;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
