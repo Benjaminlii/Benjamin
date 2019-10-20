1f8ce8579387860935eb9e39bc8bb1760a6dc94b
Benjamin <305193188@qq.com>
2019-10-20 12:08:36 +0800

    leetcode

---
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/LeetCode1.java
@@ -0,0 +1,35 @@
+package com.Benjamin.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ClassName:LeetCode1
+ * Package:com.Benjamin.leetcode
+ * <p>
+ * Description:
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * @author: Benjamin
+ * @date: 19-10-20 下午12:07
+ */
+public class LeetCode1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
