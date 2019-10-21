4b3a232dad5f7ee2c2545874e0a8850687ab2a3d
Benjamin <305193188@qq.com>
2019-10-21 23:15:57 +0800

    md

--- LeetCodeNoteBook/src/com/Benjamin/leetcode/Demo.java
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/Demo.java
@@ -12,6 +12,19 @@ package com.Benjamin.leetcode;
  */
 public class Demo {
     public static void main(String[] args) {
+
+        int cb = Integer.SIZE-3;
+
+        int running = -1 << cb;
+        int shutdown = 0 << cb;
+        int stop = 1 << cb;
+        int tidying = 2 << cb;
+        int terminated = 3 << cb;
+        System.out.println(running);
+        System.out.println(shutdown);
+        System.out.println(stop);
+        System.out.println(tidying);
+        System.out.println(terminated);
         System.out.println("hello world, ha?");
     }
 }
