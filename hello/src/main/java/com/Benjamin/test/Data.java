89ed292b00def3dd372f77ce86505c28e7cd2c59
Benjamin <305193188@qq.com>
2019-11-12 16:31:12 +0800

    insert

---
+++ hello/src/main/java/com/Benjamin/test/Data.java
@@ -0,0 +1,36 @@
+package com.Benjamin.test;
+
+/**
+ * ClassName:Data
+ * Package:com.Benjamin.test
+ * <p>
+ * Description:
+ *
+ * @author: Benjamin
+ * @date: 19-11-12 下午3:24
+ */
+public class Data {
+    private int val;
+    private int ind;
+
+    public Data(int val, int ind) {
+        this.val = val;
+        this.ind = ind;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public int getInd() {
+        return ind;
+    }
+
+    public void setInd(int ind) {
+        this.ind = ind;
+    }
+}
--- hello/src/main/java/com/Benjamin/test/MybatisFirst.java
+++ hello/src/main/java/com/Benjamin/test/MybatisFirst.java
@@ -8,40 +8,104 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MybatisFirst {
     @Test
+    public void findStudentByIdTest() throws InterruptedException {
 
-    public void findStudentByIdTest() {
-        String resource = "SqlMapConfig.xml";
-        SqlSession sqlSession = null;
-        try (
-                InputStream inputStream = Resources.getResourceAsStream(resource)
-        ) {
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        Demo demo0_100 = new Demo(0*1000, 1000);
+        Demo demo100_200 = new Demo(1*1000, 1000);
+        Demo demo200_300 = new Demo(2*1000, 1000);
+        Demo demo300_400 = new Demo(3*1000, 1000);
+        Demo demo400_500 = new Demo(4*1000, 1000);
+        Demo demo500_600 = new Demo(5*1000, 1000);
+        Demo demo600_700 = new Demo(6*1000, 1000);
+        Demo demo700_800 = new Demo(7*1000, 1000);
+        Demo demo800_900 = new Demo(8*1000, 1000);
+        Demo demo900_1000 = new Demo(9*1000, 1000);
+        Thread th1 = new Thread(demo0_100);
+        Thread th2 = new Thread(demo100_200);
+        Thread th3 = new Thread(demo200_300);
+        Thread th4 = new Thread(demo300_400);
+        Thread th5 = new Thread(demo400_500);
+        Thread th6 = new Thread(demo500_600);
+        Thread th7 = new Thread(demo600_700);
+        Thread th8 = new Thread(demo700_800);
+        Thread th9 = new Thread(demo800_900);
+        Thread th10 = new Thread(demo900_1000);
+        th1.start();
+        th2.start();
+        th3.start();
+        th4.start();
+        th5.start();
+        th6.start();
+        th7.start();
+        th8.start();
+        th9.start();
+        th10.start();
+        th1.join();
+        th2.join();
+        th3.join();
+        th4.join();
+        th5.join();
+        th6.join();
+        th7.join();
+        th8.join();
+        th9.join();
+        th10.join();
+    }
+    @Test
+    public void findStudentByIdTest1() throws InterruptedException {
 
-            sqlSession = sqlSessionFactory.openSession();
+        Demo demo0_100 = new Demo(0, 10);
+        Thread th1 = new Thread(demo0_100);
+        th1.start();
+        th1.join();
+    }
 
-            Date date = new Date();
+    class Demo implements Runnable{
 
-            for (int i = 1; i <= 10000000; i++) {
-                Map<String, Integer> paraMap = new HashMap<>();
-                paraMap.put("id", i);
-                paraMap.put("val", i);
-                paraMap.put("ind", i);
-                sqlSession.insert("insertIntoTable", paraMap);
+        private int start;
+        private int nums;
+
+        public Demo(int start, int nums) {
+            this.start = start;
+            this.nums = nums;
+        }
+
+        @Override
+        public void run() {
+            String resource = "SqlMapConfig.xml";
+            SqlSession sqlSession = null;
+            try (
+                    InputStream inputStream = Resources.getResourceAsStream(resource)
+            ) {
+                SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+                sqlSession = sqlSessionFactory.openSession();
+
+                Date date = new Date();
+
+                for (int i = start; i < start+nums; i++) {
+                    List<Data> datas = new ArrayList<>(1000);
+                    for (int j = 0; j < 1000; j++) {
+                        datas.add(new Data(i*1000+j, i*1000+j));
+                    }
+                    sqlSession.insert("insertIntoTable", datas);
+                    System.out.println(">>> " + i*1000);
+                }
+
+                System.out.println((new Date().getTime()-date.getTime())/(1000*60));
+                sqlSession.commit();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (sqlSession != null)
+                    sqlSession.close();
             }
-
-            System.out.println((new Date().getTime()-date.getTime())/(1000*60));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (sqlSession != null)
-                sqlSession.close();
         }
     }
+
 }
