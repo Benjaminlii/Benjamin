68b86d29693d6f2a478349d1fa5357c2be80df67
Benjamin <305193188@qq.com>
2019-11-23 13:27:45 +0800

    leetcode
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/leetcode/LeetCode1.java
+++ LeetCodeNoteBook/src/com/Benjamin/leetcode/LeetCode1.java
@@ -1,5 +1,6 @@
 package com.Benjamin.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,12 @@ import java.util.Map;
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
+ * <p>
+ * 思考:
+ * O(n)的解法:使用一个map,编译一遍数组,每次计算出当前遍历到的元素和result相加可以得到target,判断之前有没有得到过result
+ * 如果result已经存在在map中,就说明找到了
+ * 如果不在,就将当前遍历到的元素放入map
+ * 明显的空间换时间
  *
  * @author: Benjamin
  * @date: 19-10-20 下午12:07
@@ -27,9 +34,26 @@ public class LeetCode1 {
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] { i, map.get(complement) };
+                return new int[]{i, map.get(complement)};
             }
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result;
+        for (int i = 0; i < nums.length; i++) {
+            result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new LeetCode1().twoSum1(new int[]{2, 7, 11, 15}, 9)));
     }
 }
