8f0e1abeb7dc42920358b4cbd46ad0ec0fd162f5
Benjamin <305193188@qq.com>
2020-01-17 12:14:29 +0800

    offer
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer65.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer65.java
@@ -1,7 +1,5 @@
 package com.Benjamin.offer;
 
-import java.util.Arrays;
-
 /**
  * ClassName:Offer65
  * Package:com.Benjamin.offer
@@ -22,18 +20,38 @@ import java.util.Arrays;
  * @date: 20-1-16 下午4:39
  */
 public class Offer65 {
-    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        char[][] chars = new char[rows][cols];
-        int row = 0,col = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            if (col >= cols){
-                row++;
-                col %= cols;
-            }
-            chars[row][col] = matrix[i];
-            col++;
-        }
 
+    boolean[] visited = null;
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        visited = new boolean[matrix.length];
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < cols; j++)
+                if(subHasPath(matrix,rows,cols,str,i,j,0))
+                    return true;
+        return false;
+    }
+
+    public boolean subHasPath(char[] matrix, int rows, int cols, char[] str, int row, int col, int len){
+        if(matrix[row*cols+col] != str[len]|| visited[row*cols+col] == true){
+            return false;
+        }
+        if(len == str.length-1){
+            return true;
+        }
+        visited[row*cols+col] = true;
+        if(row > 0 && subHasPath(matrix,rows,cols,str,row-1,col,len+1)){
+            return true;
+        }
+        if(row < rows-1 && subHasPath(matrix,rows,cols,str,row+1,col,len+1)){
+            return true;
+        }
+        if(col > 0 && subHasPath(matrix,rows,cols,str,row,col-1,len+1)){
+            return true;
+        }
+        if(col < cols-1 && subHasPath(matrix,rows,cols,str,row,col+1,len+1)){
+            return true;
+        }
+        visited[row*cols+col] = false;
         return false;
     }
 
