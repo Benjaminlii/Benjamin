01de020e60c463ab7627bbf5aa51bf26af4e47ad
Benjamin <305193188@qq.com>
2020-03-13 13:25:51 +0800

    二叉搜索树的后序遍历序列
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer23.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer23.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  * Package:com.Benjamin.offer
  * <p>
  * Description:
+ * 二叉搜索树的后序遍历序列
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
  * 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
  * <p>
@@ -23,6 +24,9 @@ public class Offer23 {
         if (sequence.length == 0) {
             return false;
         }
+        if (sequence.length == 1){
+            return true;
+        }
         int root = sequence[sequence.length - 1];
         int i = 0;
         while (sequence[i] < root) {
@@ -35,9 +39,6 @@ public class Offer23 {
         }
         boolean left = VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0, i));
         boolean right = VerifySquenceOfBST(Arrays.copyOfRange(sequence, i, sequence.length-1));
-        if (sequence.length == 1){
-            return true;
-        }
         if (i == 0){
             return right;
         }
