9162ee2d4351daa549bf81910b275f969224a0f0
Benjamin <305193188@qq.com>
2020-03-13 15:06:55 +0800

    构建乘积数组
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer51.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer51.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  * Package:com.Benjamin.offer
  * <p>
  * Description:
+ * 构建乘积数组
  * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
  * <p>
  * 思路:
@@ -21,11 +22,11 @@ public class Offer51 {
     public int[] multiply(int[] A) {
         int[] b = new int[A.length];
         for (int i = 0; i < A.length; i++) {
-            b[i] = i != 0 ? A[i-1]*b[i-1] : 1;
+            b[i] = i != 0 ? A[i - 1] * b[i - 1] : 1;
         }
         int temp = 1;
-        for (int i = A.length-2; i >= 0; i--){
-            temp *= A[i+1];
+        for (int i = A.length - 2; i >= 0; i--) {
+            temp *= A[i + 1];
             b[i] *= temp;
         }
         return b;
