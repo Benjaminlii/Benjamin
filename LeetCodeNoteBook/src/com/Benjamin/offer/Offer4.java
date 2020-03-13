a883f0ac8753b3ba8ed1259b365c8e3363f3d615
Benjamin <305193188@qq.com>
2020-03-13 11:53:22 +0800

    重建二叉树
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer4.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer4.java
@@ -1,15 +1,14 @@
 package com.Benjamin.offer;
 
-import java.util.Arrays;
-
 /**
  * ClassName:Offer4
  * Package:com.Benjamin.offer
  * <p>
  * Description:
+ * 重建二叉树
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
- *
+ * <p>
  * 思路:
  * 使用递归解决
  * 每次在先序队列中寻找第一个出现在当前递归中中序队列的元素,那么这个元素就是这个中序对应的树中的根节点,向下递归,并在最后返回到上一层
@@ -37,22 +36,24 @@ public class Offer4 {
                     "\n}";
         }
 
-        public static void print_per(TreeNode root){
-            if (root != null){
+        public static void print_per(TreeNode root) {
+            if (root != null) {
                 System.out.print(root.val + " ");
                 print_per(root.left);
                 print_per(root.right);
             }
         }
-        public static void print_in(TreeNode root){
-            if (root != null){
+
+        public static void print_in(TreeNode root) {
+            if (root != null) {
                 print_in(root.left);
                 System.out.print(root.val + " ");
                 print_in(root.right);
             }
         }
-        public static void print_hou(TreeNode root){
-            if (root != null){
+
+        public static void print_hou(TreeNode root) {
+            if (root != null) {
                 print_hou(root.left);
                 print_hou(root.right);
                 System.out.print(root.val + " ");
@@ -61,26 +62,39 @@ public class Offer4 {
     }
 
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        if(in.length==0){
+        return reConstructBinaryTree(pre, in, 0, in.length - 1);
+    }
+
+    /**
+     * 自定义的递归函数
+     * @param pre 先序序列
+     * @param in 中序序列
+     * @param start 被分割的中序序列的首元素下标
+     * @param end 被分割的中序序列的尾元素下标
+     * @return 这个被分割的中序序列对应的树结构
+     */
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in, int start, int end) {
+        // 相等时,说明是叶子节点,还要继续往下进行
+        if (start > end) {
             return null;
         }
-        // 循环遍历pre,找到第一个能在in中找到的元素
-        int rootSub = 0;
+
+        // 这里初始化为start,实际上不会出现后面找不到rootSub的情况
+        int rootSub = start;
+        // 在先序序列中找到第一个出现在被分割的中序序列中的元素,作为分割元素
+        MARK:
         for (int i : pre) {
-            boolean flag = false;
-            for (rootSub = 0; rootSub < in.length; rootSub++) {
-                if (i == in[rootSub]){
-                    flag = true;
-                    break;
+            // 注意这个for中间都是相等
+            for (rootSub = start; rootSub <= end; rootSub++) {
+                if (i == in[rootSub]) {
+                    break MARK;
                 }
             }
-            if (flag){
-                break;
-            }
         }
+        // 建立根节点,向左右递归
         TreeNode root = new TreeNode(in[rootSub]);
-        root.left = reConstructBinaryTree(pre, Arrays.copyOfRange(in, 0, rootSub));
-        root.right = reConstructBinaryTree(pre, Arrays.copyOfRange(in, rootSub+1, in.length));
+        root.left = reConstructBinaryTree(pre, in, start, rootSub - 1);
+        root.right = reConstructBinaryTree(pre, in, rootSub + 1, end);
         return root;
     }
 
