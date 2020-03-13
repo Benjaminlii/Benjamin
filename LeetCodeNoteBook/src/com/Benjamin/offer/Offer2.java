01af62685e13ff79c2328942994c2bc2a01ba847
Benjamin <305193188@qq.com>
2020-03-13 10:45:37 +0800

    替换空格
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer2.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer2.java
@@ -7,6 +7,8 @@ package com.Benjamin.offer;
  * Description:
  * 替换空格
  * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ * 思路:
+ * 调函数,或者使用StringBuffer
  *
  * @author: Benjamin
  * @date: 19-11-18 上午9:38
