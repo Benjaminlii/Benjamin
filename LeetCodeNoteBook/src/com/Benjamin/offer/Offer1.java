9e7677635778fa59c1078e0a2313efa6f0dd3b97
Benjamin <305193188@qq.com>
2020-03-13 10:40:25 +0800

    二维数组中的查找
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer1.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer1.java
@@ -6,6 +6,7 @@ package com.Benjamin.offer;
  * 剑指offer的刷题记录,本无序,这里按照热度指数降序排列
  * <p>
  * Description:
+ * 二维数组中的查找
  * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * <p>
@@ -16,6 +17,10 @@ package com.Benjamin.offer;
  * 暴力法优化
  * 多次二分 nlogn
  * 确定行的二分(在最右边找到最后一个小于等于targe的值,最左边找到最后一个小于target)
+ * 最优解:
+ * 从左下角开始,判断num(i,j下标处)和target的关系
+ * 如果target<num,那么i--
+ * 如果target>num,那么j++
  *
  * @author: Benjamin
  * @date: 19-11-18 上午8:36
