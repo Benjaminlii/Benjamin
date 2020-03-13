8236b423688d7ac16e91dc8cb673a7b6a569b52f
Benjamin <305193188@qq.com>
2020-03-13 12:38:15 +0800

    树的子结构
    Signed-off-by: Benjamin <305193188@qq.com>

--- LeetCodeNoteBook/src/com/Benjamin/offer/Offer17.java
+++ LeetCodeNoteBook/src/com/Benjamin/offer/Offer17.java
@@ -5,7 +5,17 @@ package com.Benjamin.offer;
  * Package:com.Benjamin.offer
  * <p>
  * Description:
+ * 树的子结构
  * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ *
+ * 思路:
+ * 分三条路往下走:
+ * 判断当街节点的数是否和给出的树结构一样
+ * 左子树
+ * 右子树
+ *
+ * 在判断两棵树是否一样时,两棵树都不为null时,判断val,如果有为null的节点,那么如果是root2,则返回正确
+ * 因为root2只可以提前结束的
  *
  * @author: Benjamin
  * @date: 19-11-27 上午11:06
@@ -23,25 +33,12 @@ public class Offer17 {
         }
     }
 
-    // 判断子树
-    public boolean HasSubtree1(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
-            return true;
-        } else if (root1 == null) {
-            return false;
-        }
-        if (root1.val == root2.val) {
-            return HasSubtree(root1.left, root2.left) && HasSubtree(root1.right, root2.right);
-        }
-        return HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
-    }
-
     // 子结构不代表子树
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
         if (root1 == null || root2 == null) {
             return false;
         }
-        return HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2) || doesTree1EqualsTree2(root1, root2);
+        return doesTree1EqualsTree2(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
     }
 
     // roo2可以条件结束,root1不行
@@ -52,7 +49,9 @@ public class Offer17 {
                 return doesTree1EqualsTree2(root1.left, root2.left) && doesTree1EqualsTree2(root1.right, root2.right);
             }
             return false;
-        } else return root2 == null;
+        } else {
+            return root2 == null;
+        }
     }
 
     public static void main(String[] args) {
