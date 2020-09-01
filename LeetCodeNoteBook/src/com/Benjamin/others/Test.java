2ffdfb2e051655cb0444bbd9a581638b3fcce14a
Benjamin <benjamin_leee@163.com>
2020-09-01 21:02:38 +0800

    add

--- LeetCodeNoteBook/src/com/Benjamin/others/Test.java
+++ LeetCodeNoteBook/src/com/Benjamin/others/Test.java
@@ -7,19 +7,7 @@ package com.Benjamin.others;
  * @Date: 2020.7.11
  */
 public class Test {
-    public static int getNumber() {
-        int num;
-
-        try {
-            num = 1;
-            return num;
-        } finally {
-            num = 2;
-            return num;
-        }
-    }
 
     public static void main(String[] args) {
-        System.out.println(getNumber());
     }
 }
